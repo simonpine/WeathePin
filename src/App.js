58063c63408b7d0ce798285a19e0825318b82594
Simon Pineda <simonpineda0521@gmail.com>
2022-11-22 12:28:11 -0500

    Cree la aplicacion, y el ambiente de dasrollo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const key = '44b1a28f33497d44372ca1c66e49be29'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>sibero</code> and save to reload.
         </p>
         <a
           className="App-link"
