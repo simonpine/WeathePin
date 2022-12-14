cc594d2685b55f69982b4fe329dff4fca62935e2
Simon Pineda <simonpineda0521@gmail.com>
2022-12-14 10:52:50 -0500

    Now the app is responsive

--- src/components/input.js
+++ src/components/input.js
@@ -4,36 +4,38 @@ function Input( {getWeather, setUnits, setUnits2, setUnits3, colorForAll, colorF
     return (
         <form style={colorForAll2} className="search" onSubmit={getWeather}>
             <input className="searchWrite" id="cityInput" placeholder='New York'></input>
-            <button className="searchButton" type="submit" value='search'><img className="lup" src="icons/search.png" ></img></button>
-            <button className="searchButton" onClick={()=>{ 
-                setUnits('imperial') 
-                setUnits2('F')
-                setUnits3('mph')
-                }} type="submit" value='search'>°F</button>
-            <button className="searchButton" onClick={()=>{ 
-                setUnits('metric') 
-                setUnits2('C') 
-                setUnits3('km/h')
-                }} type="submit" value='search'>°C</button>
-            <button className="searchButton" onClick={()=>{ 
-                if(colorForAll.backgroundColor === 'rgba(0, 0, 0, 0.568)'){
-                    color({
-                        backgroundColor: 'rgba(255,255,255, 0.3)',
-                    },
-                    {
-                        filter: 'invert(100%)',
+            <div className="buttons">
+                <button className="searchButton" type="submit" value='search'><img className="lup" src="icons/search.png" ></img></button>
+                <button className="searchButton" onClick={()=>{ 
+                    if(colorForAll.backgroundColor === 'rgba(0, 0, 0, 0.568)'){
+                        color({
+                            backgroundColor: 'rgba(255,255,255, 0.3)',
+                        },
+                        {
+                            filter: 'invert(100%)',
+                        }
+                        )
                     }
-                    )
-                }
-                else{
-                    color({
-                        backgroundColor: 'rgba(0, 0, 0, 0.568)',
-                    },
-                    {
-                        filter: 'none',
-                    })
-                }
-                }}><img className="lup" src="icons/dark.png" ></img></button>
+                    else{
+                        color({
+                            backgroundColor: 'rgba(0, 0, 0, 0.568)',
+                        },
+                        {
+                            filter: 'none',
+                        })
+                    }
+                    }}><img className="lup" src="icons/dark.png" ></img></button>
+                <button className="searchButton" onClick={()=>{ 
+                    setUnits('imperial') 
+                    setUnits2('F')
+                    setUnits3('mph')
+                    }} type="submit" value='search'>°F</button>
+                <button className="searchButton" onClick={()=>{ 
+                    setUnits('metric') 
+                    setUnits2('C') 
+                    setUnits3('km/h')
+                    }} type="submit" value='search'>°C</button>
+            </div>
         </form>
     );
   }
